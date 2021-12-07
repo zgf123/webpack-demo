22baed8204a8a90f654feeaf6e5d04171000acb4
zhangguangfei <zhangguangfei@bizseer.com>
2021-12-07 18:56:29 +0800

    feat: 02

--- 02AssetManagement/src/index.js
+++ 02AssetManagement/src/index.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
+import './style.css'
+import Pic from './pic.jpg'
+import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello')
+
+  console.log(Pic);
+
+  // 将图像添加到我们现有的 div。
+  var myPic = new Image();
+  myPic.src = Pic;
+
+  element.appendChild(myPic);
+
+  console.log(Data);
 
   return element;
 }
--- 02AssetManagement/webpack.config.js
+++ 02AssetManagement/webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const consoleRed = '\x1b[91m'
 const consoleGreen = '\033[32m'
 
-
 console.log(consoleGreen, '-------------------');
 console.log(consoleRed, path.join(__dirname, 'bar'))
 console.log(consoleRed, path.resolve(__dirname, 'bar'))
@@ -13,5 +12,29 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      }
+    ]
   }
 };
