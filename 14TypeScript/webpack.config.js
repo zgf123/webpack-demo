080b3659ed67659d210f82b62078969c5ec8d271
zhangguangfei <zhangguangfei@bizseer.com>
2021-12-17 15:07:53 +0800

    feat: typescript

---
+++ 14TypeScript/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const consoleRed = '\x1b[91m'
+const consoleGreen = '\033[32m'
+
+
+console.log(consoleGreen, '-------------------');
+console.log(consoleRed, '14TypeScript')
+console.log(consoleGreen, '-------------------');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx&/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      title: 'TypeScript'
+    }),
+  ]
+};
--- README.md
+++ README.md
@@ -27,3 +27,5 @@
 #### 12、Lazy Loading [懒加载](https://webpack.docschina.org/guides/lazy-loading/)
 
 #### 13、Shimming [Shimming 预置依赖](https://webpack.docschina.org/guides/shimming/)
+
+#### 14、TypeScript [TypeScript](https://webpack.docschina.org/guides/typescript/)
